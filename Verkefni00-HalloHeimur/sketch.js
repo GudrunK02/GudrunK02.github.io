e14e86ba519491494de44281828e1d83afcf26d9
GudrunK02 <gkh0707@gmail.com>
2021-03-18 10:43:18 +0000

    Breyta bakgrunni

--- Verkefni00-HalloHeimur/sketch.js
+++ Verkefni00-HalloHeimur/sketch.js
@@ -2,21 +2,11 @@
 
 function setup() {
   createCanvas(400, 400);
-  background(255,200,0);
+  background(0,255,0);
   frameRate(4);
 }
 
 function draw() {
   fill(random(0,255));
   ellipse(200,200,50,50);
-}
-
-function setup(){
-    createCanvas(300,300);
-    background(0,255,0);
-    fill(150);
-    ellipse(100,100,50,50);
-    fill(0);
-    textSize(20);
-    text("Halló heimur",200,40);
 }
