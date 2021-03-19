7682e8eda701c064fe481016cd63ae743267e51b
GudrunK02 <gkh0707@gmail.com>
2021-03-19 13:32:46 +0000

    litir

--- Verkefni00-HalloHeimur/sketch.js
+++ Verkefni00-HalloHeimur/sketch.js
@@ -1,11 +1,17 @@
 // Hér kemur kóðinn þinn:
 function setup(){
-    createCanvas(500,500);
-    background(200,0,0);
-    fill(150);
-    ellipse(100,100,50,50);
+    createCanvas(1000,600);
+    background(250,0,250);
+    fill(0,0,200);
+    ellipse(100,100,100,100);
+    fill(250,0,0)
+    ellipse(100,400,70,70);
+    fill(0,250,0)
+    ellipse(400,100,50,50);
+    fill(0)
+    rect(100,400,200)
     fill(0);
     textSize(50);
-    text("Halló heimur",100,200);
+    text("Halló heimur",100,250);
 
 }
