3cc44cd5b75ffc0d45ba6f68fdb613cc25f14522
GudrunK02 <gkh0707@gmail.com>
2021-03-22 15:38:06 +0000

    kassar og eth fun

--- Verkefni00-HalloHeimur/sketch.js
+++ Verkefni00-HalloHeimur/sketch.js
@@ -2,17 +2,22 @@
 function setup(){
     createCanvas(1000,600);
     background(250,0,250);
+    textSize(50);
+    text("Halló heimur",100,250);
     fill(0,0,200);
     ellipse(100,100,100,100);
     fill(250,0,0)
     ellipse(100,400,70,70);
     fill(0,250,0)
     ellipse(400,100,50,50);
+    fill(208,245,27)
+    rect(600,150,150,80);
+    fill(71,206,188)
+    stroke(250,0,0)
+    rect(280,355,550,140);
     fill(0)
-    rect(100,400,200)
-    fill(0);
-    textSize(50);
-    text("Halló heimur",100,250);
     textSize(75);
     text("Góðann daginn",300,450);
+
+
 }
