2175b7f0e080732e8633b895a9fa28872a6d7c43
GudrunK02 <gkh0707@gmail.com>
2021-03-24 14:55:22 +0000

    01-rauður kassi og 2xsvartar línur

--- Verkefni01-Mondrain/sketch.js
+++ Verkefni01-Mondrain/sketch.js
@@ -1,10 +1,13 @@
 // Hér kemur kóðinn þinn:
 function setup(){
-    createCanvas(1000,500);
-    background(102,178,255);
-    fill(150);
-    ellipse(100,100,50,50);
-    fill(0);
-    textSize(20);
-    text("Núna er ég leið",200,40);
+    createCanvas(1500,800);
+    background(0);
+    fill(255);
+    rect(0,0,600,600);
+    fill(215,0,0)
+    rect(0,0,254,319)
+    fill(0)
+    rect(255,0,12,600);
+    rect(0,320,600,12);
+
 }
