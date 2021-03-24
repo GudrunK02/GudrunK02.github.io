4c0ca2896521b0db49a0a6ad1615a088fa5a361f
GudrunK02 <gkh0707@gmail.com>
2021-03-24 21:39:58 +0000

    mondrain-tilbúið

--- Verkefni01-Mondrain/sketch.js
+++ Verkefni01-Mondrain/sketch.js
@@ -2,12 +2,28 @@
 function setup(){
     createCanvas(1500,800);
     background(0);
-    fill(255);
+    //hvítur bakgrunnur
+    fill(225,239,237);
     rect(0,0,600,600);
+    //rauður kassi
     fill(215,0,0)
     rect(0,0,254,319)
+    //svartar línur
     fill(0)
     rect(255,0,12,600);
     rect(0,320,600,12);
+    rect(268,563,282,14);
+    rect(547,333,13,266);
+    rect(559,427,40,20);
+    rect(422,578,12,22);
+    //blár kassi
+    fill(34,64,146)
+    rect(558,332,43,96);
+    //gulur kassi
+    fill(248,221,18)
+    rect(267,578,155,21);
+    //grár kassi
+    fill(35,35,38)
+    rect(432,578,114,21);
 
 }
