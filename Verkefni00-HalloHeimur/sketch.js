b37d0e21ad33d3dd5ac893c9fe26813965059078
GudrunK02 <gkh0707@gmail.com>
2021-03-24 14:39:36 +0000

    its back:)

--- Verkefni00-HalloHeimur/sketch.js
+++ Verkefni00-HalloHeimur/sketch.js
@@ -2,5 +2,28 @@
 function setup(){
     createCanvas(1500,800); //bakgrunnur
     background(102,178,255);
+    //texti 1
+    textSize(50);
+    fill(0)
+    text("Halló heimur",100,250);
+    //hringir og rétthyrningar
+    fill(0,0,200);
+    ellipse(100,100,100,100);
+    fill(250,0,0)
+    ellipse(100,400,70,70);
+    fill(0,250,0)
+    ellipse(400,100,50,50);
+    fill(208,245,27)
+    rect(600,150,150,80);
+    fill(102,0,51)
+    rect(70,252,350,15);
+    //texti 2 og rétthyrningur utan um hann
+    fill(153,0,153)
+    rect(280,355,550,140);
+    stroke(0,255,255)
+    fill(0)
+    textSize(75);
+    text("Góðan daginn",300,450);
+
 
 }
