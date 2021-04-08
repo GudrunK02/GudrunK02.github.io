d3d1f63eee149ed02d39bb54835c5a7e2d43bf9d
GudrunK02 <gkh0707@gmail.com>
2021-04-08 10:34:24 +0000

    02

--- Verkefni02-lifandi mynd/sketch.js
+++ Verkefni02-lifandi mynd/sketch.js
@@ -9,21 +9,25 @@ function setup(){
 
     background(47,67,97);
       stroke(105,93,49);
+      //sól
       strokeWeight(25)
         fill(242,160,27)
     ellipse(150,270,110,110);
+    //fjöll
       strokeWeight(2)
       fill(30,20,20);
     triangle(200,300,0,500,400,500);
     triangle(900,250,600,500,1200,500);
+    fill(50,45,40);
+  triangle(500,200,200,500,800,500);
+    //hraun
       fill(255,0,0);
-    ellipse(random(350,600),random(250,100), 30,30);
+    ellipse(random(360,600),random(240,100), 30,30);
       fill(194,70,0);
-    ellipse(random(350,600),random(250,100),35,25);
+    ellipse(random(360,600),random(240,100),35,25);
       fill(170,0,0)
-    rect(random(350,600),random(250,100),20,40);
-      fill(50,45,40);
-    triangle(500,200,200,500,800,500);
+    rect(random(360,600),random(240,100),20,40);
+    //hraun þríhyrningur
       noStroke();
       fill(194,70,33);
     triangle(500,200,425,275,575,275);
