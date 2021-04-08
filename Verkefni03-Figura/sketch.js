8d02630452b65f82f7b586a14dd56cf08af867dd
GudrunK02 <gkh0707@gmail.com>
2021-04-08 11:56:37 +0000

    03-svin byrjun

--- Verkefni03-Figura/sketch.js
+++ Verkefni03-Figura/sketch.js
@@ -1,5 +1,5 @@
 // Hér kemur kóðinn þinn:
-var bukur = 150;
+var bukur = 200;
 
 function setup() {
   createCanvas(1000,600);
@@ -9,19 +9,31 @@ function draw() {
   background(122,126,170);
   // Teiknum búkinn
   fill(236,36,94);
-  ellipse(mouseX,mouseY,200,150,bukur);
+  ellipse(mouseX,mouseY,250,220,bukur);
   // Teiknum nefið
   fill(255);
-  ellipse(mouseX - 1/bukur,mouseY + 15, 100,80);
-  // Teiknum augun
+  ellipse(mouseX - 1/bukur,mouseY + 25, 110,90);
+  // Teiknum nasir
   fill(100);
-  ellipse (mouseX - 20 - 1/bukur,mouseY + 20 , 15,30);
-  ellipse (mouseX + 20 - 1/bukur,mouseY + 20, 15,30);
+  ellipse (mouseX - 20 - 1/bukur,mouseY + 25 , 20,20);
+  ellipse (mouseX + 20 - 1/bukur,mouseY + 25, 20,20);
+  // Teiknum augu
+  fill(100);
+  ellipse (mouseX - 45 - 1/bukur,mouseY - 40 , 30,40);
+  ellipse (mouseX + 55 - 1/bukur,mouseY - 40, 30,40);
+  // Teiknum augasteina
+  fill(255);
+  ellipse (mouseX - 51 - 1/bukur,mouseY - 46 , 9,9);
+  ellipse (mouseX + 49 - 1/bukur,mouseY - 46, 9,9);
+  // Teiknum eyru
+  stroke(255,0,0)
+  triangle(mouseX - 51 - 1/bukur,mouseY - 46 , 200)
+  triangle(mouseX + 49 - 1/bukur,mouseY - 46, 200)
 
 }
 
 function mousePressed() {
-	bukur = random (150,150);
+	bukur = random (200,200);
 }
 
 // Teiknum fæturna
