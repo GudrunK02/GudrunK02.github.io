e3caf081664ebd07e1687ffe4a5ed313c526dac3
GudrunK02 <gkh0707@gmail.com>
2021-04-14 14:57:05 +0000

    breytingar á 03 og byrjun á 04

--- Verkefni03-Figura/sketch.js
+++ Verkefni03-Figura/sketch.js
@@ -1,5 +1,10 @@
 // Hér kemur kóðinn þinn:
 var bukur = 200;
+var nasir = 20;
+var augasteinn1 = 46
+var augasteinn2 = 54
+var munnurX = 60
+var munnurY = 15
 
 function setup() {
   createCanvas(1000,600);
@@ -10,22 +15,22 @@ function draw() {
   noStroke()
   // Teiknum búkinn
   fill(255,153,153);
-  ellipse(mouseX,mouseY,250,220,bukur);
+  ellipse(mouseX,mouseY,250,220);
   // Teiknum nefið
   fill(255,204,204);
   ellipse(mouseX - 1/bukur,mouseY + 25, 110,90);
   // Teiknum nasir
   fill(100);
-  ellipse (mouseX - 20 - 1/bukur,mouseY + 25 , 20,20);
-  ellipse (mouseX + 20 - 1/bukur,mouseY + 25, 20,20);
+  ellipse (mouseX - 20 - 1/bukur,mouseY + 25,nasir,nasir);
+  ellipse (mouseX + 20 - 1/bukur,mouseY + 25,nasir,nasir);
   // Teiknum augu
   fill(100);
   ellipse (mouseX - 45 - 1/bukur,mouseY - 40 , 30,40);
   ellipse (mouseX + 55 - 1/bukur,mouseY - 40, 30,40);
   // Teiknum augasteina
   fill(255);
-  ellipse (mouseX - 51 - 1/bukur,mouseY - 46 , 9,9);
-  ellipse (mouseX + 49 - 1/bukur,mouseY - 46, 9,9);
+  ellipse (mouseX - augasteinn1 - 1/bukur,mouseY - 40 - 1/bukur,11,11);
+  ellipse (mouseX + augasteinn2 - 1/bukur,mouseY - 40 - 1/bukur,11,11);
   // Teiknum eyru
   fill(255,153,153)
   triangle(mouseX - 110- 1/bukur,mouseY - 30- 1/bukur,mouseX-50- 1/bukur,mouseY-100- 1/bukur,mouseX-120- 1/bukur,mouseY-130- 1/bukur)
@@ -34,12 +39,16 @@ function draw() {
   fill(255,204,204)
   triangle(mouseX-100-1/bukur,mouseY-70-1/bukur,mouseX-70-1/bukur,mouseY-95-1/bukur,mouseX-105-1/bukur,mouseY-110- 1/bukur)
   triangle(mouseX+100-1/bukur,mouseY-70-1/bukur,mouseX+70-1/bukur,mouseY-95-1/bukur,mouseX+105-1/bukur,mouseY-110- 1/bukur)
+    //Teiknum munn
+    fill(100)
+  arc(mouseX -1/bukur,mouseY -1/bukur +80 ,munnurX,munnurY,TWO_PI,PI)
+
 }
 
 function mousePressed() {
-	bukur = random (200,200);
+  nasir = random (15,25);
+  augasteinn1 = random (38,52)
+  augasteinn2 = random (47,61)
+  munnurX = random (60,80)
+  munnurY = random (15,30)
 }
-
-// Teiknum fæturna
-line (mouseX - 20, mouseY + bukur/2, mouseX - 20, mouseY + bukur/2 + 50);
-line (mouseX + 20, mouseY + bukur/2, mouseX + 20, mouseY + bukur/2 + 50);
