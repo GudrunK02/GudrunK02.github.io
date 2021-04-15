d4af94e9754c754764fe0edf9b5f6fe317c69d0d
GudrunK02 <gkh0707@gmail.com>
2021-04-15 11:50:42 +0000

    04

--- Verkefni04-Lykkjuaskorun/sketch.js
+++ Verkefni04-Lykkjuaskorun/sketch.js
@@ -1,15 +1,18 @@
 // Hér kemur kóðinn þinn:
 function setup(){
-  createCanvas(400,400);
-  background(255, 215, 0);
-  fill(0);
-  colorMode(HSB);
-  frameRate(2);
-}
-
-function draw(){
-  fill(random(0,360),75,100);
-  for(var i = 0; i < 5 ; i = i + 1) {
-    ellipse(100, 50 + i*20, 15, 15);
+    createCanvas(400,400);
+    background(255,215,0);
+  	fill(0);
+    colorMode(HSB);
+    frameRate(2);
   }
-}
+
+  function draw() {
+    strokeWeight(2)
+    for(var x = 10; x < width; x = x+20){
+      fill(random(160,360),75,100);
+    for(var i = -2; i < 20 ; i = i + 1) {
+        ellipse(x, 50 + i*20, 15, 15);
+      }
+    }
+  }
