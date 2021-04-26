fef5bd6ae0af7fbc1f19b8722f0b80ebd5dc7b1a
GudrunK02 <gkh0707@gmail.com>
2021-04-26 15:37:33 +0000

    05-lokið í bili

--- Verkefni05-Lykkjulist/sketch.js
+++ Verkefni05-Lykkjulist/sketch.js
@@ -1,1 +1,32 @@
 // Hér kemur kóðinn þinn:
+function setup() {
+  createCanvas(1500,900);
+  background(255,255,0);
+  noStroke();
+  frameRate(2);
+  colorMode(HSB);
+}
+
+function draw() {
+  for(var x = 400; x < 1030; x = x+30){
+    for(var y=200; y < 600; y = y+30){
+      fill(random(30, 100));
+      rect(x,y, 30,30);
+  }
+  }
+
+  for(var x = 400; x < 1090; x = x+90){
+    for(var y=200; y < 630; y = y+30){
+      ellipse(x,y,45,50);
+      noFill();
+      stroke(255,0,0)
+
+  }
+  }
+  for(var x = 400; x < 1044; x = x+22){
+    for(var y=200; y < 660; y = y+60){
+      fill(random(0,360),40,150);
+      ellipse(x,y, 30,15);
+	}
+  }
+}
