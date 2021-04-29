02b1b996c07b24cf7c324e9b4bb71d9df99b03e5
GudrunK02 <gkh0707@gmail.com>
2021-04-29 10:58:16 +0000

    06- tilbúið

--- Verkefni06-Skjaskipting/sketch.js
+++ Verkefni06-Skjaskipting/sketch.js
@@ -1,33 +1,35 @@
 // Hér kemur kóðinn þinn:
 function setup() {
-  createCanvas(800,800);
-  frameRate(5);
+  createCanvas(1440,750);
+  background(250,150,250);
 }
 
+function keyPressed() {
+  if(keyCode == 32){
+background(250,150,250);
+
+    }
+  }
 function draw() {
-  background(255,200,0);
-  if(mouseX < width/2 && mouseY < height/2){
-  	fill(94,36,236);
-    for(var i = 1; i< 50;i = i+1) {
-      ellipse(random(width/2), random(height/2), random(30), random(50));
+  if(mouseX < width/2 && mouseY < height/2 && !mouseIsPressed){
+  	fill(66,135,245);
+    ellipse(mouseX, mouseY, 50, 50);
+  }
+
+   if(mouseX > width/2 && mouseY < height/2 && !mouseIsPressed){
+  	fill(232,58,23);
+    for(var j = 1; j< 50;j = j+1) {
+      rect(mouseX,mouseY, 50, 50);
     }
   }
-   if(mouseX > width/2 && mouseY < height/2){
-  	fill(236,36,94);
+   if(mouseX > width/2 && mouseY > height/2 && !mouseIsPressed){
+  	fill(66,135,245);
     for(var j = 1; j< 50;j = j+1) {
-      rect(random(width/2,width),random(height/2), random(30), random(30));
-    }
-  }
-   if(mouseX > width/2 && mouseY > height/2){
-  	fill(94,36,236);
-    for(var j = 1; j< 50;j = j+1) {
-      rect(random(width/2,width),random(height/2,height), random(30), random(30));
+      rect(mouseX,mouseY, 50, 50);
    }
   }
-  if(mouseX < width/2 && mouseY > height/2){
-  	fill(236,36,94);
-    for(var j = 1; j< 50;j = j+1) {
-      ellipse(random(width/2),random(height/2,height), random(30), random(30));
+  if(mouseX < width/2 && mouseY > height/2 && !mouseIsPressed){
+  	fill(232,58,23);
+    ellipse(mouseX,mouseY,50,50);
    }
   }
-}
